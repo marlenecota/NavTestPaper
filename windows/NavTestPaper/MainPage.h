033dee0c8bae6e2204d4dff1116eadd7d604e1ff
Marlene Cota <1422161+marlenecota@users.noreply.github.com>
2024-07-16 17:43:04 -0700

    Paper - min react-navigation/drawer app

---
+++ windows/NavTestPaper/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::NavTestPaper::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::NavTestPaper::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
